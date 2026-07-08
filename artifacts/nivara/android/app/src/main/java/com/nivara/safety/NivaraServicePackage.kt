6e7be19384d826f541e8e8c13d18885ec71efc66
Navya <navyapriyabaid@gmail.com>
2026-07-08 11:28:59 +0000

    Remove committed android folder, use prebuild

--- artifacts/nivara/android/app/src/main/java/com/nivara/safety/NivaraServicePackage.kt
+++
@@ -1,6 +0,0 @@
-package com.nivara.safety
-import com.facebook.react.ReactPackage; import com.facebook.react.bridge.*; import com.facebook.react.uimanager.ViewManager
-class NivaraServicePackage : ReactPackage {
-    override fun createNativeModules(ctx: ReactApplicationContext): List<NativeModule> = listOf(NivaraServiceModule(ctx))
-    override fun createViewManagers(ctx: ReactApplicationContext): List<ViewManager<*, *>> = emptyList()
-}
